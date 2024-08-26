bc74ad41fba3e3b55c3d6673cb6d3c00ba0bcb0c
arekasek <arekas2914@wp.pl>
2024-08-26 19:14:21 +0200

    version 0.2

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-6 bg-gray-100">
       <h1 className="text-3xl font-bold mb-6 text-center">Select Genre</h1>
-      <div className="flex space-x-4">
+      <div className="flex gap-4 flex-wrap justify-center items-center">
         <div
           className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 cursor-pointer"
           onClick={() => handleSelectGenre("Pop")}
