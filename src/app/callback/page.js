11727ef00bd1b082a0e70523c4b2790c77820c89
Arek <arekas2914@wp.pl>
2025-09-26 15:47:28 +0200

    ROUTING

---
+++ src/app/callback/page.js
@@ -0,0 +1,29 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function Callback() {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const hash = window.location.hash
+        .substring(1)
+        .split("&")
+        .reduce((acc, item) => {
+          const [key, value] = item.split("=");
+          acc[key] = decodeURIComponent(value);
+          return acc;
+        }, {});
+
+      if (hash.access_token) {
+        localStorage.setItem("spotify_token", hash.access_token);
+        router.push("/genre");
+      } else {
+        router.push("/");
+      }
+    }
+  }, [router]);
+
+  return <p className="text-white">Logging in...</p>;
+}
